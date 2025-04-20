d5a402f9c1177ce3789bcf9d6211f23af91549a2
ranimRefaie <ranemalre@gmail.com>
2025-04-20 20:29:58 +0300

    first commit

--- src/pages/AuthPages/AuthPageLayout.tsx
+++ src/pages/AuthPages/AuthPageLayout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import GridShape from "../../components/common/GridShape";
-import { Link } from "react-router";
+// import GridShape from "../../components/common/GridShape";
+// import { Link } from "react-router";
 import ThemeTogglerTwo from "../../components/common/ThemeTogglerTwo";
 
 
@@ -10,7 +10,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative flex flex-col h-screen bg-white dark:bg-gray-900 overflow-hidden">
+    <div className="relative flex flex-col h-screen bg-white dark:bg-red-400 overflow-hidden">
       {/* Header with Logo (for mobile) */}
       {/* <header className="lg:hidden mt-6 px-6">
         <Link to="/" className="block mx-auto w-fit">
@@ -28,29 +28,7 @@ export default function AuthLayout({
       </header> */}
 
       {/* Main Content */}
-      <main className="flex flex-col justify-center  flex-1 lg:flex-row h-full">
-        {/* Form Section */}
-        <section className="flex flex-col justify-center  flex-1 px-6 lg:p-0">
-          {children}
-        </section>
 
-        {/* Graphic Section (Desktop only) */}
-        <section className="hidden lg:flex items-center justify-center w-1/2 bg-brand-950 dark:bg-white/5">
-          <div className="relative flex items-center justify-center z-1">
-            <GridShape />
-            <div className="flex flex-col items-center max-w-xs">
-              <Link to="/" className="block mb-4">
-                <img
-                  width={231}
-                  height={48}
-                  src="/images/logo/auth-logo.svg"
-                  alt="Logo"
-                />
-              </Link>
-            </div>
-          </div>
-        </section>
-      </main>
 
       {/* Theme Toggler */}
       <div className="fixed z-50 bottom-6 right-6">
