40b184f73fad90f1d925020cd5186b61b8c720b0
ranimRefaie <ranemalre@gmail.com>
2025-04-20 17:30:15 +0300

    first commit

--- src/pages/AuthPages/AuthPageLayout.tsx
+++ src/pages/AuthPages/AuthPageLayout.tsx
@@ -12,7 +12,7 @@ export default function AuthLayout({
   return (
     <div className="relative flex flex-col h-screen bg-white dark:bg-gray-900 overflow-hidden">
       {/* Header with Logo (for mobile) */}
-      <header className="lg:hidden pt-6 px-6">
+      <header className="lg:hidden mt-6 px-6">
         <Link to="/" className="block mx-auto w-fit">
           <img
             className="dark:hidden w-36"
@@ -28,9 +28,9 @@ export default function AuthLayout({
       </header>
 
       {/* Main Content */}
-      <main className="flex flex-col flex-1 lg:flex-row">
+      <main className="flex flex-col justify-center  flex-1 lg:flex-row h-full">
         {/* Form Section */}
-        <section className="flex flex-col md:justify-center  flex-1 p-6 lg:p-0">
+        <section className="flex flex-col justify-center  flex-1 p-6 lg:p-0">
           {children}
         </section>
 
