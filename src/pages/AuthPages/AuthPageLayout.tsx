78183ceb60ba39120095d3bc8235e5d72b6b428d
ranimRefaie <ranemalre@gmail.com>
2025-04-20 20:36:05 +0300

    first commit

--- src/pages/AuthPages/AuthPageLayout.tsx
+++ src/pages/AuthPages/AuthPageLayout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-// import GridShape from "../../components/common/GridShape";
-// import { Link } from "react-router";
+import GridShape from "../../components/common/GridShape";
+import { Link } from "react-router";
 import ThemeTogglerTwo from "../../components/common/ThemeTogglerTwo";
 
 
@@ -28,7 +28,29 @@ export default function AuthLayout({
       </header> */}
 
       {/* Main Content */}
+      <main className="flex flex-col justify-center  flex-1 lg:flex-row h-full">
+        {/* Form Section */}
+        <section className="flex flex-col justify-center">
+          {children}
+        </section>
 
+        {/* Graphic Section (Desktop only) */}
+        <section className="hidden lg:flex items-center justify-center w-1/2 bg-brand-950 dark:bg-white/5">
+          <div className="relative flex items-center justify-center z-1">
+            <GridShape />
+            <div className="flex flex-col items-center max-w-xs">
+              <Link to="/" className="block mb-4">
+                <img
+                  width={231}
+                  height={48}
+                  src="/images/logo/auth-logo.svg"
+                  alt="Logo"
+                />
+              </Link>
+            </div>
+          </div>
+        </section>
+      </main>
 
       {/* Theme Toggler */}
       <div className="fixed z-50 bottom-6 right-6">
